240f1f7cdaeccd74b29890286ec4d99a166a8dc6
louise <louise.grankarlsson@gmail.com>
2021-11-12 11:24:13 +0100

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,198 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Todo {
+  final String syssla;
+  final bool kryssbox;
+//
+  Todo(this.syssla, this.kryssbox);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
+  @override //för att kunna överlagra och göra min egna build funktion
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainView(),
+      theme: ThemeData(primarySwatch: Colors.pink),
+      // home: SecondView(),
     );
   }
 }
+//----------KLASSEN FÖR FÖRSTA SIDAN -----------------
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+class MainView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('TIG169 TODO'),
+          toolbarHeight: 70,
+        ),
+        body: _list(),
+        floatingActionButton: FloatingActionButton(
+            tooltip: 'Add',
+            child: Icon(Icons.add),
+            backgroundColor: Colors.pink,
+            onPressed: () {
+              //KOLLA PÅ DETTA
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SecondView()));
+            }));
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Widget _list() {
+  List<Todo> sysslor = [
+    Todo('Diska', true),
+    Todo('Städa', true),
+    Todo('Laga mat', false),
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  return ListView.builder(
+      itemBuilder: (context, index) => _RadiListan(sysslor[index]),
+      itemCount: 3);
+  // return ListView(children: sysslor.map((text) => _item(text)).toList(),);
+}
+
+// Widget _item(text) {
+//   return Row();
+// return Padding(
+//   padding: const EdgeInsets.all(20.0),
+//   child: Text(text,
+//       style: TextStyle(fontSize: 20)), //SKALL JAG HA EN CHECKBOX HÄR??
+// Checkbox(
+//   value: false,
+//   onChanged: (val) {},)
+
+Widget _RadiListan(Todo todo) {
+  var textDecoration = TextDecoration.none;
+  if (todo.kryssbox) {
+    textDecoration = TextDecoration.lineThrough;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Checkbox(
+        value: todo.kryssbox,
+        onChanged: (val) {},
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      Expanded(
+        child: Text(
+          todo.syssla,
+          style: TextStyle(
+            fontSize: 20,
+            decoration: textDecoration,
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      IconButton(
+        icon: Icon(Icons.highlight_remove),
+        onPressed: (null),
+      )
+    ],
+  );
+}
+
+// Widget _appBar2() {
+//  return Container(
+//     //  _menu(),
+//    children: [
+//       icon: Icon(Icons.more_vert)
+//       leading: const IconButton(
+//          icon: Icon(Icons.more_vert), //TRE PRICKAR SOM SKALL VARA MED I MAINVIEW
+//          tooltip: 'Navigation menu',
+//          onPressed: null, ),
+//      height: 70,
+//      decoration:  BoxDecoration(color: Colors.pink.shade200),
+//      child: Center( child: Text('TIG169 TODO',style: TextStyle(color: Colors.black, fontSize: 22),),
+
+//      )
+
+//  );
+//    ;
+//  ;
+
+// }
+
+Widget _menu() {
+  return Row(
+    children: [
+      Icon(Icons.person),
+    ],
+  );
+}
+
+// Widget _checkboxRow(_list()) {  //sätta in en lista här som argument
+// return Row(
+//   children: [
+//     Checkbox(
+//       value: false,
+//       onChanged: (val) { },
+//     ),
+//     Text('Laga mat och diska'),
+//     Text('$sysslor'),
+//   ],
+// );
+//   }
+
+//----------KLASSEN FÖR ANDRA SIDAN-------------------
+
+class SecondView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('TIG169 TODO'),
+        toolbarHeight: 70,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            height: 24,
+          ),
+          //  _appBar(),
+          Container(
+            margin: EdgeInsets.only(left: 20, top: 40),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 20),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'What are you going to do?',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ), //STÖRRE BOKSTÄVER
+          _addRow(),
+        ],
+      )),
+    );
+  }
+
+//appbar längst upp på sidan
+  Widget _appBar() {
+    return Container(
+        height: 70,
+        decoration: BoxDecoration(color: Colors.pink.shade200),
+        child: Center(
+          child: Text(
+            'TIG169 TODO',
+            style: TextStyle(color: Colors.black, fontSize: 22),
+          ),
+        ));
+  }
+
+  Widget _addRow() {
+    return Container(
+      margin: EdgeInsets.only(left: 20, top: 20),
+      child: Text('+ ADD'),
     );
   }
 }
